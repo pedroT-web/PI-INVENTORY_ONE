273b2b90a68acda352e8766a0eb0427812679613
chinagliadev <156460261+chinagliadev@users.noreply.github.com>
2026-02-12 15:42:41 -0300

    inserindo modal de editar

--- js/produto.js
+++ js/produto.js
@@ -3,3 +3,9 @@ const modal = document.getElementById('modalProduto')
 modal.addEventListener('show.bs.modal', ()=>{
     console.log('ola')
 })
+
+const modalDetalhes = document.getElementById('modalDetalhesProduto')
+
+modalDetalhes.addEventListener('show.bs.modal', ()=>{
+    console.log('ola')
+})
