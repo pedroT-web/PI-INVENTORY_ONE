d1608ad8bb63a1b5038862737c79b2e7dc936dcc
pedroT-web <pedrotaroco1@gmail.com>
2026-03-11 21:52:15 -0300

    Adicionando os parametros do .env na conexão com o banco

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 require('dotenv').config()
+
 const express = require('express')
 const app = express()
+
 const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
 const cors = require('cors')
 app.use(cors())
 app.use(express.json())
@@ -11,18 +14,18 @@ app.use(express.json())
 
 let mysql = require('mysql')
 let conexao = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "inventoryone"
+    host: `${process.env.HOST}`,
+    user: `${process.env.USER}`,
+    password: `${process.env.PASSWORD}`,
+    database: `${process.env.DATABASE}`
 })
-    conexao.connect(function (erro) {
-if (erro) {
-console.log("Deu ruim na conexão \n");
-throw erro;
-} else {
-console.log("Conexão deu bom \n")
-}
+conexao.connect(function (erro) {
+    if (erro) {
+        console.log("Deu ruim na conexão \n");
+        throw erro;
+    } else {
+        console.log("Conexão deu bom \n")
+    }
 })
 
 app.listen(3000)
